fab8019f65f2d6825f7f6505a5b824355195de87
Steven Edwards <winehacker@gmail.com>
2023-10-16 21:09:57 -0700

    Add some tests an various bits of research

--- linux/rfcomm-client/rfcomm-client.c
+++ linux/rfcomm-client/rfcomm-client.c
@@ -1,66 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include <bluetooth/rfcomm.h>
 
-int main() {
-    inquiry_info *ii = NULL;
-    int max_rsp = 255; // Maximum number of devices to discover
-    int num_rsp;
-    int dev_id, sock;
-    char addr[19] = "XX:XX:XX:XX:XX:XX"; // Replace with the peripheral's MAC address
+int main(int argc, char **argv)
+{
+    struct sockaddr_rc addr = { 0 };
+    int s, status;
+    char dest[18] = "3C:22:FB:9A:01:4F"; // Mac
 
-    // Prepare a "Hello" message
-    char hello_msg[] = "Hello";
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    dev_id = hci_get_route(NULL);
-    sock = hci_open_dev(dev_id);
+    // set the connection parameters (who to connect to)
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t)1;
+    str2ba(dest, &addr.rc_bdaddr);
 
-    if (dev_id < 0 || sock < 0) {
-        perror("Cannot open Bluetooth socket");
-        exit(1);
+    // connect to the server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    if (status < 0) {
+        perror("Connection error");
+        return 1;
     }
 
-    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-
-    num_rsp = hci_inquiry(dev_id, 8, max_rsp, NULL, &ii, IREQ_CACHE_FLUSH);
-
-    if (num_rsp < 0) {
-        perror("Bluetooth inquiry failed");
-    }
-
-    for (int i = 0; i < num_rsp; i++) {
-        ba2str(&(ii + i)->bdaddr, addr);
-
-        if (strcmp(addr, "XX:XX:XX:XX:XX:XX") == 0) { // Replace with the target peripheral's MAC address
-            // Establish an RFCOMM socket connection and send the "Hello" message
-            int s, status;
-            struct sockaddr_rc addr = { 0 };
-            char dest[18] = "XX:XX:XX:XX:XX:XX"; // Replace with the peripheral's MAC address
-
-            s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-
-            addr.rc_family = AF_BLUETOOTH;
-            addr.rc_channel = (uint8_t)1; // RFCOMM channel to use
-            str2ba(dest, &addr.rc_bdaddr);
-
-            status = connect(s, (struct sockaddr*)&addr, sizeof(addr));
-            if (status < 0) {
-                perror("Connection failed");
-            }
-
-            // Send the "Hello" message
-            send(s, hello_msg, sizeof(hello_msg), 0);
-
-            close(s);
+    // send a message
+    if (status == 0) {
+        status = write(s, "hello!", 6);
+        if (status < 0) {
+            perror("Write error");
+        } else {
+            printf("Message sent: hello!\n");
         }
+    } else {
+        perror("Connection error");
     }
 
-    free(ii);
-    close(sock);
+    // Read and print messages from the server
+    char buffer[1024];
+    int bytesRead;
+    while (1) {
+        bytesRead = read(s, buffer, sizeof(buffer));
+        if (bytesRead <= 0) {
+            // The server may have closed the connection, or an error occurred
+            break;
+        }
+        printf("Received: %.*s", bytesRead, buffer);
+    }
 
+    close(s);
     return 0;
 }
 
